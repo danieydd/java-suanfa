3c0c772a573d8b6811a2a2250a5f21c2bde54587
danie <fanfanzhibeiydd@163.com>
2022-03-14 11:47:08 +0800

    外观数列

---
+++ src/com/suanfa/demo/temp/leetcode/leetcodebook/primary/string/Test13.java
@@ -0,0 +1,64 @@
+package com.suanfa.demo.temp.leetcode.leetcodebook.primary.string;
+
+/**
+ * @ClassName Test13
+ * @Description 外观数列
+ * 给定一个正整数 n ，输出外观数列的第 n 项。
+ * <p>
+ * 「外观数列」是一个整数序列，从数字 1 开始，序列中的每一项都是对前一项的描述。
+ * <p>
+ * 你可以将其视作是由递归公式定义的数字字符串序列：
+ * <p>
+ * countAndSay(1) = "1"
+ * countAndSay(n) 是对 countAndSay(n-1) 的描述，然后转换成另一个数字字符串。
+ * 前五项如下：
+ * <p>
+ * 输入：n = 4
+ * 输出："1211"
+ * 解释：
+ * countAndSay(1) = "1"
+ * countAndSay(2) = 读 "1" = 一 个 1 = "11"
+ * countAndSay(3) = 读 "11" = 二 个 1 = "21"
+ * countAndSay(4) = 读 "21" = 一 个 2 + 一 个 1 = "12" + "11" = "1211"
+ * 1 <= n <= 30
+ * @Author danie
+ * @Date 2022-03-11 17:10
+ */
+public class Test13 {
+
+
+    public static String countAndSay(int n) {
+        // 递归出口
+        if (n == 1) {
+            return "1";
+        }
+        // 假设我们获得上一次的结果为 s1 = 112213
+        String s1 = countAndSay(n - 1);
+        // 定义结果
+        StringBuilder result = new StringBuilder();
+        // 对s1遍历处理获取值
+        char local = s1.charAt(0);
+        int count = 0;
+        for (int i = 0; i < s1.length(); i++) {
+            // 设定计数器 计算同一个数字出现的次数 count
+            if (s1.charAt(i) == local) {
+                count++;
+            } else {
+                // 不符合，记录下
+                result.append(count);
+                result.append(local);
+                count = 1;
+                local = s1.charAt(i);
+            }
+        }
+        result.append(count);
+        result.append(local);
+        return result.toString();
+
+    }
+
+    public static void main(String[] args) {
+        String s = countAndSay(4);
+        System.out.println(s);
+    }
+}
