c9abca31a770a4cc4cd4f9cd5cf3a7c80249837e
danie <fanfanzhibeiydd@163.com>
2022-03-28 18:51:19 +0800

    反转链表   双链表+递归

--- src/com/suanfa/demo/temp/leetcode/leetcodebook/primary/linkedList/Test06.java
+++ src/com/suanfa/demo/temp/leetcode/leetcodebook/primary/linkedList/Test06.java
@@ -1,8 +1,5 @@
 package com.suanfa.demo.temp.leetcode.leetcodebook.primary.linkedList;
 
-import java.util.Objects;
-import java.util.Stack;
-
 /**
  * @ClassName Test03
  * @Description 反转链表
@@ -10,36 +7,23 @@ import java.util.Stack;
  * 输入：head = [1,2,3,4,5]
  * 输出：[5,4,3,2,1]
  * 链表可以选用迭代或递归方式完成反转。你能否用两种方法解决这道题？
- * tips：
- * 利用栈
+ * Tips:
+ * 递归
  * @Date 2022-03-24 16:41
  */
-public class Test04 {
+public class Test06 {
     public static ListNode reverseList(ListNode head) {
-        Stack<ListNode> stack = new Stack<>();
-        while (Objects.nonNull(head)) {
-            stack.push(new ListNode(head.val));
-            head = head.next;
-        }
-        ListNode listNode = setNewNode(stack);
-        return listNode;
+        //退出条件
+        if (head == null || head.next == null) return head;
+        //逻辑
+        ListNode next = head.next;
+        //递归
+        ListNode reverse = reverseList(next);
+        //逻辑
+        next.next = head;
+        head.next = null;
+        return reverse;
     }
-
-    public static ListNode setNewNode(Stack<ListNode> stack) {
-        if (stack.isEmpty())
-            return null;
-        ListNode node = stack.pop();
-        ListNode dummy = node;
-        while (!stack.isEmpty()) {
-            ListNode tempNode = stack.pop();
-            node.next = tempNode;
-            node = node.next;
-        }
-//        node.next = null;
-        return dummy;
-
-    }
-
 
     public static void main(String[] args) {
         ListNode node = new ListNode(4);
